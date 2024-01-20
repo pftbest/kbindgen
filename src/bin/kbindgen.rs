d884e7e3583c20134f9986eaa271047388086ed2
Vadzim Dambrouski <pftbest@gmail.com>
2024-01-20 12:56:47 +0100

    Pass file name as a cli argument

--- src/bin/kbindgen.rs
+++ src/bin/kbindgen.rs
@@ -1,17 +1,13 @@
 use kbindgen::Tokenizer;
 
 fn main() {
-    let file_name = "/Volumes/CaseSens/rust-linux/rust/bindings/bindings_generated.rs.preproc";
+    let file_name = std::env::args().nth(1).unwrap();
     let text = std::fs::read(file_name).unwrap();
 
-    let mut tk;
-    let mut tokens;
-    for i in 0..100 {
-        tk = Tokenizer::new(text.as_slice().into());
-        tokens = tk.tokenize();
-    }
+    let mut tk = Tokenizer::new(text.as_slice().into());
+    let tokens = tk.tokenize();
 
-    // for t in tokens.iter().take(100) {
-    //     println!("{:?}", t.text);
-    // }
+    for t in tokens.iter().take(100) {
+        println!("{:?}", t.text);
+    }
 }
